9afe488bc0fb9a4b8f24bd113d8190d13ffea2e1
Champ <76679390+DrChamp1@users.noreply.github.com>
2021-03-25 12:12:33 +0530

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,3 +1,5 @@
+----------QBCore----------------[Change into your core]
+
 Champ = nil
 TriggerEvent('Champ:GetObject', function(obj) Champ = obj end)
 
@@ -10,3 +12,21 @@ AddEventHandler('knb:mech:pay', function()
 	Player.Functions.RemoveMoney('cash', 250)
     end
 end)
+
+
+
+---------------ESX---------------
+--[[
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('knb:mech:pay')
+AddEventHandler('knb:mech:pay', function()
+    local source = source
+    local Player =  ESX.GetPlayerFromId(source)
+
+    if Player.getMoney > 250 then
+    Player.RemoveMoney('cash', 250)
+    end
+end)
+--]]
